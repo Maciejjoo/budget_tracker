f7710f38bf9e2805e3cefbd590adb92b6b78c77e
Maciej Januszek <maciej.januszek9@gmail.com>
2026-02-18 16:13:01 +0100

    SafeCubit instead of cubit

--- lib/src/application/theme/theme_cubit.dart
+++ lib/src/application/theme/theme_cubit.dart
@@ -1,8 +1,8 @@
+import 'package:budget_tracker/src/utils/cubit/safe_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class ThemeCubit extends Cubit<ThemeMode> {
+class ThemeCubit extends SafeCubit<ThemeMode> {
   static const String _themeKey = 'theme_mode';
   final SharedPreferences _prefs;
 
